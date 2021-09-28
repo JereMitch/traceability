1b002f78d1d67f6a2ad72b8051e2f0b1c4dfa351
JereMitch <jere.mitch8@gmail.com>
2021-09-28 14:54:58 -0600

    fixed typo

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const Rollbar = require('rollbar')
 const rollbar = new Rollbar({
     accessToken: 'ffe70e0ef1564c6b996867b5f7fdba18',
     captureUncaught: true,
-    captureUnhandledRejections: true
+    captureUnhandledRejections: true,
 })
 
 app.use(express.static("client"))
