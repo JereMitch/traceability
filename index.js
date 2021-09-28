b674b7567f72e9f4184a26d62ec7414ce1f71e7f
JereMitch <jere.mitch8@gmail.com>
2021-09-28 15:24:32 -0600

    because courtney said so

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const rollbar = new Rollbar({
 app.use(express.static("client"))
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'))
+    res.sendFile(path.join(__dirname, './client/index.html'))
     rollbar.info('Success')
 })
 
