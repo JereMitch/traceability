24ad188b09380a35945594d657e928d83238742a
JereMitch <jere.mitch8@gmail.com>
2021-09-28 15:29:49 -0600

    Try another thing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const rollbar = new Rollbar({
     captureUnhandledRejections: true,
 })
 
-app.use(express.static("client"))
+// app.use(express.static("client"))
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, './client/index.html'))
